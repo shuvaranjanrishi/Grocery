bab926fcfda9425823c94eec8d537778e26e03f4
shuvaranjanrishi <shopnershopnil44@gmail.com>
2022-01-12 19:45:11 +0000

    login page design done

--- app/src/main/java/com/shuvaranjanrishi/grocery/LoginActivity.java
+++ app/src/main/java/com/shuvaranjanrishi/grocery/LoginActivity.java
@@ -3,12 +3,26 @@ package com.shuvaranjanrishi.grocery;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    //UI Views
+    private EditText emailEt, passwordEt;
+    private TextView forgotTv, noAccountTv;
+    private Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        emailEt = findViewById(R.id.emailEt);
+        passwordEt = findViewById(R.id.passwordEt);
+        forgotTv = findViewById(R.id.forgotTv);
+        noAccountTv = findViewById(R.id.noAccountTv);
+        loginBtn = findViewById(R.id.loginBtn);
     }
 }
